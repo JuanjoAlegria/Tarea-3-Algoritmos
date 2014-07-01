94be3194d5f302a81cf7f878f29d106b2c5f4e54
José <joseg.njoy@gmail.com>
2014-07-01 10:20:43 -0400

    Main

--- Tarea-3-Algoritmos/src/MST.java
+++ Tarea-3-Algoritmos/src/MST.java
@@ -1,7 +1,6 @@
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.tree.TreeNode;
 
 
 public class MST {
--- Tarea-3-Algoritmos/src/Main.java
+++ Tarea-3-Algoritmos/src/Main.java
@@ -7,44 +7,48 @@ import java.util.StringTokenizer;
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
-		String file = "dj38.tsp";
-	    FileReader inputFile = new FileReader(file);
-	    BufferedReader bufferReader = new BufferedReader(inputFile);
-	    String line;
-	    int i=1;
-	    ArrayList<Point> points = new ArrayList<Point>();
-	    while ((line = bufferReader.readLine()) != null)   {
-    		StringTokenizer tokens = new StringTokenizer(line);
-    		String first = tokens.nextToken();
-    		if(isNumber(first)){
-	    		double x = Double.parseDouble(tokens.nextToken());
-	    		double y = Double.parseDouble(tokens.nextToken());
-	    		
-	    		points.add(new Point(x,y));
-    		}
-	    }
-	    bufferReader.close();
-	    
-	    long time_start;
-		long time_stop;
-		
-		time_start = System.nanoTime();
-		ArrayList<Point> res = NearestNeighbor.execute(points);
-		time_stop = System.nanoTime();
-		System.out.println((time_stop - time_start) * Math.pow(10, -9));
-		double total=0;
-		for(Point p : res){
-			Point p2;
-			if(res.indexOf(p) == res.size()-1){
-				p2 = res.get(0);
+		//String[] files = {"wi29","dj38","qa194","uy734","zi929","mu1979","ca4663","ja9847","gr9882","fi10369","it16862","vm22775","sw24978"};
+		String[] files = {"gr9882","fi10369","it16862","vm22775","sw24978"};
+		String[] countryName = {"Western Sahara","Djibouti","Qatar","Uruguay","Zimbabwe","Oman","Canada","Japan","Greece","Finland","Italy","Vietnam","Sweden"};
+		for(String file : files){
+		    FileReader inputFile = new FileReader(file + ".tsp");
+		    BufferedReader bufferReader = new BufferedReader(inputFile);
+		    String line;
+		    ArrayList<Point> points = new ArrayList<Point>();
+		    while ((line = bufferReader.readLine()) != null)   {
+	    		StringTokenizer tokens = new StringTokenizer(line);
+	    		String first = tokens.nextToken();
+	    		if(isNumber(first)){
+		    		double x = Double.parseDouble(tokens.nextToken());
+		    		double y = Double.parseDouble(tokens.nextToken());
+		    		
+		    		points.add(new Point(x,y));
+	    		}
+		    }
+		    bufferReader.close();
+		    
+		    long time_start;
+			long time_stop;
+			
+			time_start = System.nanoTime();
+			ArrayList<Point> res = NearestNeighbor.execute(points);
+			time_stop = System.nanoTime();
+			System.out.println("Pais: " + file);
+			System.out.println("Time [s]: " + (time_stop - time_start) * Math.pow(10, -9));
+			double total=0;
+			for(Point p : res){
+				Point p2;
+				if(res.indexOf(p) == res.size()-1){
+					p2 = res.get(0);
+				}
+				else{
+					p2 = res.get(res.indexOf(p)+1);
+				}
+				double d1 = calculateDistance(p,p2);
+				total+=d1;
 			}
-			else{
-				p2 = res.get(res.indexOf(p)+1);
-			}
-			double d1 = calculateDistance(p,p2);
-			total+=d1;
+			System.out.println("Distance: " + total);
 		}
-		System.out.println(total);
 		
 	}
 	
